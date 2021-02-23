15adb17408d4e0caca23699eac3e0376b2f4422b
wonder_princess <sekai_love.me.do@hotmail.co.jp>
2021-02-23 11:41:44 +0900

    一応完成

--- sortAlgorithm.swift
+++ sortAlgorithm.swift
@@ -1,48 +1,76 @@
-/*
-var (str1, str2, str3, str4) = ("", "", "", "")
+    var arrayInt:[Int] = [8, 1, 5, 9, 6, 2]
+    var arrayStr:[String] = ["あ", "c", "を", "a"]
+    
+    print(arrayStr)
+//    sortString(arrayStr)
+    
 
-str1 = "あ"
-str2 = "を"
-str3 = "a"
-str4 = "A"
+    
+    print("ソートする文字列「,」で区切ってを入力してください")
+    print("文字列と数字を混ぜたら殺す")
+    
+    let getString:String = ""    
+    
+    print("昇順 → 1")
+    print("降順 → 2")
+    
+    let getSortType:String = ""
+/*    
+    let separatedStr:[String] = readStr(String)
+    
 
-for code in str1.unicodeScalars
-{
-    print(code.value)
-}
-
-for code in str2.unicodeScalars
-{
-    print(code.value)
-}
-
-for code in str3.unicodeScalars
-{
-    print(code.value)
-}
-
-for code in str4.unicodeScalars
-{
-    print(code.value)
-}
-
-
-print(str1 + str2 + str3)
+    /*----- コロコロ仕様を変えるなハゲ -------------
+    ------- 現行の空白削除とセパレートは -----------
+    ------- どうすればいいんだよ -----------------*/
+    func readStr(_ str: String) -> [String] {
+        str = str.remove(characterSet: .whitespaces)
+        let arr:[String] = str.components(separatedBy: ",")
+        return arr
+    }
 */
 
-var arrayInt :[Int] = [8, 1, 5, 9, 6, 2]
-var arrayStr :[String] = ["あ", "c", "を", "a"]
-
-print(arrayStr)
-sortString(arrayStr)
-
-    func sortString(_ arrayStr :[String]) 
+    if getSortType == "1"{
+        do { try sortInt(arrayStr, false) }
+        catch {sortString(arrayStr, false) }
+    }
+    
+    if getSortType == "2"{
+        do { try sortInt(arrayStr, true) }
+        catch {sortString(arrayStr, true) }
+    }
+    
+    func sortInt(_ arrInt:[String], _ des:Bool)
     {
-        var arr: Array<Int> = changeUnicodeInt(arrayStr)
+        var arr:Array<Int> = changeInt(arrInt)
+        arr = bubbleSort(arr)
+        if des {
+            print(descending(arr))
+        }else{
+        print(arr)
+        }
+    }
+    
+    func sortString(_ arrayStr:[String], _ des:Bool) 
+    {
+        var arr:Array<Int> = changeUnicodeInt(arrayStr)
         arr = bubbleSort(arr)
 //        var dic: Dictionary<Int, String> = changeUnicodeDic(arrayStr)
+//        arr = changeChar(arr)
+        if des {
+            print(descending(arr))
+        }else{
         print(arr)
-        changeChar(arr)
+        }
+    }
+    
+    func changeInt(_ arr:[String]) -> [Int]
+    {
+        var arrInt: Array<Int> = Array<Int>()
+        for num in arr
+        {
+            arrInt.append(Int(num)!)
+        }
+        return arrayInt
     }
     
     
@@ -79,9 +107,9 @@ sortString(arrayStr)
 
     
     
-    func changeChar(_ arr :[Int])
+    func changeChar(_ arr:[Int])
     {
-        var arrUInt32: Array<Any> = Array<Any>()
+        var arrUInt32:Array<Any> = Array<Any>()
         
         for num in arr
         {
@@ -91,7 +119,6 @@ sortString(arrayStr)
 //            print(a)
 
             var uint32 = UInt32(num)
-            var str = uint32.map(Character.init).map(String.init)
 
             
 /*
@@ -106,13 +133,13 @@ sortString(arrayStr)
     }
 
 
-    func bubbleSort(_ arr :[Int]) -> [Int] {
-        var arr :[Int] = arr
+    func bubbleSort(_ arr:[Int]) -> [Int] {
+        var arr:[Int] = arr
         for x in arr{
-            var y :Int = 0
+            var y:Int = 0
             for y in 0..<arr.count-1{
                 if arr[y] > arr[y+1]{
-                    var num :Int = arr[y]
+                    var num:Int = arr[y]
                     arr[y] = arr[y+1]
                     arr[y+1] = num
                 }
@@ -120,5 +147,14 @@ sortString(arrayStr)
         }
         return arr
     }
-
-//print(bubbleSort(arrayInt))
+    
+    func descending(_ arr :[Any]) -> [Any] {
+        var desArr:Array<Any> = Array<Any>()
+        var count:Int = arr.count-1
+        for i in 0..<arr.count-1
+        {
+            desArr.append(arr[count])
+            count -= 1
+        }
+        return desArr
+    }
